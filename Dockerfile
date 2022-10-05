275c6330c7dc1d8917ca367a97fd27a81c646954
adamkali <adamkali@outlook.com>
2022-10-05 13:26:23 -0400

    Test Script

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#                          Stage 1: Build the application
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 FROM golang:1.18-alpine AS builder
 
 # make the directory for the source code
@@ -16,29 +20,15 @@ WORKDIR /
 
 # copy the source code to the working directory
 COPY ./Tavern-Backend/ /go/src/Tavern-Backend/
-
-# get TavernProfile-env from the private repo
-# Get the the env vars from github secrets
-ARG GITHUB_PASS
-ARG GITHUB_USER
-# update and upgrade the packages
-RUN apk update && apk upgrade
-# install git
-RUN apk add git
-# clone the repo
-# use the github username and password to clone the repo
-RUN git clone https://${GITHUB_USER}:${GITHUB_PASS}@github.com/${GITHUB_USER}/TavernProfile-env.git
-
-COPY ./Tavern-Backend/TavernProfile-env/ /go/src/Tavern-Backend/env/
 COPY ./Tavern-Backend/env/ /Files/env/
 
 WORKDIR /go/src/Tavern-Backend
 
 # Now it should build
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o /Files/TavernProfile
-# RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o /TavernProfile
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#                          Stage 2: Create the final image
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 # create alpine image to run the application 
--- cmd/build.sh
+++ cmd/build.sh
@@ -3,11 +3,47 @@
 # Get the commit message
 COMMIT_MESSAGE=$1
 
+# Setup a progress bar
+PUR='\033[0;35m'
+BLU='\033[0;34m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+STAGE0 = "Pulling main brach from git"
+STAGE1 = "Updating VERSION"
+STAGE2 = "Logging into AWS ECR"
+STAGE3 = "Building docker image"
+STAGE4 = "Tagging docker image"
+STAGE5 = "Pushing docker image to ECR"
+STAGE6 = "COMPLETE!"
+
+PROG0 = "[$BLU=>$PUR--------------------------------------$NCR] 0%"
+PROG1 = "[$BLU######$PUR=>--------------------------------$NCR] 20%"
+PROG2 = "[$BLU##############$PUR=>------------------------$NCR] 40%"
+PROG3 = "[$BLU######################$PUR=>----------------$NCR] 60%"
+PROG4 = "[$BLU##############################$PUR=>--------$NCR] 80%"
+PROG5 = "[$BLU######################################$PUR=>$NCR] 100%"
+
+echo -e "$PUR$STAGE0$NC$PROG0"
+# git add -A
+# git commit -m "$COMMIT_MESSAGE"
+# then checkout the Beor and throw away output to avoid printing it
+git checkout Beor 2>&1 > /dev/null
+git merge main 2>&1 > /dev/null
+
+git add -A 2>&1 > /dev/null
+git commit -m "$COMMIT_MESSAGE" 2>&1 > /dev/null
+
+# git push origin beor
+git push origin Beor 2>&1 > /dev/null
+
+echo -e "$PUR$STAGE1$NC$PROG1"
+
 # check if there is a VERSION.yaml file
-# if not make one
+# if not make one throw away any output to the terminal
 if [ ! -f ./VERSION.yaml ]; then
-    echo "major: 0" > ./VERSION.yaml
-    echo "minor: 1" >> ./VERSION.yaml
+    echo "major: 0" >  ./VERSION.yaml 2>&1 > /dev/null
+    echo "minor: 1" >> ./VERSION.yaml 2>&1 > /dev/null
 fi
 
 # check if -M is set
@@ -31,17 +67,25 @@ else
     MINOR=$((MINOR+1))
 fi
 
-# git add -A
-# git commit -m "$COMMIT_MESSAGE"
-# then checkout the Beor
-git checkout Beor
-git merge main
+# update the VERSION.yaml file and throw away any output to the terminal
+echo "major: $MAJOR" >  ./VERSION.yaml 2>&1 > /dev/null
+echo "minor: $MINOR" >> ./VERSION.yaml 2>&1 > /dev/null
 
-git add -A
-git commit -m "$COMMIT_MESSAGE"
+# build the docker image and throw away any output to the terminal
+echo -e "$PUR$STAGE2$NC$PROG2"
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 739810740537.dkr.ecr.us-east-1.amazonaws.com 2>&1 > /dev/null
 
-# git push origin beor
-git push origin Beor
+echo -e "$PUR$STAGE3$NC$PROG3"
+docker build -t tavern-profile-beor . 2>&1 > /dev/null
+
+echo -e "$PUR$STAGE4$NC$PROG4"
+docker tag tavern-profile-beor:latest 739810740537.dkr.ecr.us-east-1.amazonaws.com/tavern-profile-beor:$MAJOR.$MINOR 2>&1 > /dev/null
+
+echo -e "$PUR$STAGE5$NC$PROG5"
+docker push 739810740537.dkr.ecr.us-east-1.amazonaws.com/tavern-profile-beor:$MAJOR.$MINOR 2>&1 > /dev/null
+
 
 # git checkout main
-git checkout main
+git checkout main 2>&1 > /dev/null
+
+echo -e "$PUR$STAGE6$NC"
