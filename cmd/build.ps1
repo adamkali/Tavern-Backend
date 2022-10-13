036764c7064295c29f355bb997f3b70010af84e5
Adam Kalinowski <Adam>
2022-10-13 14:49:06 -0400

    no message

--- cmd/build.ps1
+++ cmd/build.ps1
@@ -137,14 +137,14 @@ Write-Host "`r${STAGE4}" -ForegroundColor Magenta
 # print the progress bar
 Write-Host " ${PROG4}" -ForegroundColor Magenta
 # tag the docker image and throw away output to avoid printing it
-docker tag "tavern-profile-beor:latest 739810740537.dkr.ecr.us-east-1.amazonaws.com/tavern-profile-beor:$MAJOR.$MINOR" | Out-Null || quit "Failed to tag docker image"
+docker tag tavern-profile-beor:latest 739810740537.dkr.ecr.us-east-1.amazonaws.com/tavern-profile-beor:$MAJOR.$MINOR | Out-Null || quit "Failed to tag docker image"
 
 # print the sixth stage in purple
 Write-Host "`r${STAGE5}" -ForegroundColor Magenta
 # print the progress bar
 Write-Host " ${PROG5}" -ForegroundColor Magenta
 # push the docker image and throw away output to avoid printing it
-docker push "739810740537.dkr.ecr.us-east-1.amazonaws.com/tavern-profile-beor:$MAJOR.$MINOR" | Out-Null || quit "Failed to push docker image to ECR"
+docker push 739810740537.dkr.ecr.us-east-1.amazonaws.com/tavern-profile-beor:$MAJOR.$MINOR | Out-Null || quit "Failed to push docker image to ECR"
 
 # print the seventh stage in purple
 Write-Host "`r${STAGE6}" -ForegroundColor Magenta
